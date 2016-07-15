b9768d5960c90b57218580f79d7195b20445f714
XanderOxido <proshowrock@gmail.com>
2016-07-15 21:34:33 +0200

    Add files via upload

---
+++ ejercicio_1_ruby/exercise1.rb
@@ -0,0 +1,21 @@
+
+
+
+
+#Ejercicio 1 de nuestro precioso ruby.rb
+
+puts 'What is the source file?'
+
+source_file = gets.chomp
+
+puts'What is the destination file?'
+
+destination_file = gets.chomp
+
+source_file_contents = IO.read(source_file)
+
+IO.write(destination_file, source_file_contents)
+
+
+
+
